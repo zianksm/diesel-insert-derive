66984eb819d3ac7b466436ff665cf212ae5efd11
zian <mynameiszian@gmail.com>
2023-05-14 23:48:51 +0800

    works

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,3 +1,11 @@
+use diesel::Insertable;
+
+
+#[diesel_insert_derive::auto_insert(table_name = some_table)]
+pub struct TestTable {
+    id: i64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Testing");
 }
